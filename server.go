d02106e798a7451abae95ba74ea12af79a3adb3b
lichong <chong.li@archermind.com>
2023-02-23 23:07:11 +0800

    增加全局消息广播

--- server.go
+++ server.go
@@ -1,52 +1,99 @@
 package main
 
+/**
+Server类
+**/
+
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
-// 创建一个结构体
+// Server结构体
 type Server struct {
-	Ip   string
-	Port int
+	Ip        string
+	Port      int
+	OnlineMap map[string]*User //在线用户列表（因为在线用户表具有全局属性，需要加锁）
+	mapLock   sync.RWMutex     //读写锁
+	Message   chan string      //消息广播的channle
 }
 
-// new一个server对象，然后返回
+// new一个server对象
 func NewServer(ip string, port int) *Server {
 
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 
 	return server
 }
 
-func (this *Server) handler(conn net.Conn) {
-	//...当前连接的业务
-	fmt.Println("链接建立成功")
+// 监听Message消息管道，一旦有消息就发送给全部在线用户
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+
+		//将消息发送给全部在线用户
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg //将消息发送到用户消息管道
+		}
+		this.mapLock.Unlock()
+	}
 }
 
-// 启动服务器的接口(套接字
+// 广播消息
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
+// Handler业务方法
+func (this *Server) handler(conn net.Conn) {
+
+	user := NewUser(conn)
+
+	//用户上线，将用户加入到nolineMap中
+	this.mapLock.Lock() //加锁
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock() //释放锁
+
+	//广播当前用户上线消息
+	this.BroadCast(user, "已上线")
+
+	//阻塞hanler
+	select {}
+}
+
+// 启动Server
 func (this *Server) Start() {
 
-	//1、创建socket listen
+	//创建socket listen
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
 	if err != nil {
 		fmt.Println("net.Listen err:", err)
 		return
 	}
-	//2、阻塞等待接收
+
+	// 关闭 listen socket
+	defer listener.Close()
+
+	//启动监听Message属性的goroutine
+	go this.ListenMessager()
+
+	//阻塞等待接收
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("listener accpet err:", err)
 			continue
 		}
-		//3、do handler，返回的链接发送到业务函数
+		//do handler，返回的链接发送到业务函数
 		go this.handler(conn)
 	}
 
-	//4、关闭socket
-	defer listener.Close()
 }
--- user.go
+++ user.go
@@ -2,22 +2,41 @@ package main
 
 import "net"
 
+/*
+*
+用户类
+*
+*/
 type User struct {
-	Name string
-	Ip string
-	C chan int
-	conn net.Conn
+	Name string      //名称
+	Addr string      //ip地址
+	C    chan string //消息管道
+	conn net.Conn    //客户端链接
 }
 
+// new一个用户对象
 func NewUser(conn net.Conn) *User {
-	//获取当前客户的连接地址
+	//获取客户端ip地址
 	userAddr := conn.RemoteAddr().String()
 
 	user := &User{
-		Name:userAddr,
-		Ip:userAddr,
-		C:make(chan stirng),
-		conn:conn,
+		Name: userAddr,
+		Addr: userAddr,
+		C:    make(chan string),
+		conn: conn,
 	}
+
+	//启动监听当前user的消息管道goroutine
+	go user.ListenMessage()
+
 	return user
+}
+
+// 监听当前user的消息channel
+func (this *User) ListenMessage() {
+	//持续监听
+	for {
+		msg := <-this.C                     //从管道中获取消息
+		this.conn.Write([]byte(msg + "\n")) //发送byte类型数组
+	}
 }
